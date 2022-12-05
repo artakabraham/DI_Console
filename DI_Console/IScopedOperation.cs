a0a48fd8dcfd67741f1291086a9bd1966763b776
Artak Abrahamyan <Artak.Abrahamyan@medfarsolutions.com>
2022-12-05 13:31:55 +0400

    remove unused usings

--- DI_Console/IScopedOperation.cs
+++ DI_Console/IScopedOperation.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DI_Console
+﻿namespace DI_Console
 {
     internal interface IScopedOperation : IOperation
     {
