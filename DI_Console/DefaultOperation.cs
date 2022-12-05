72a1e113ee9ae7094be61c7207aaa68ea6e39920
Artak Abrahamyan <Artak.Abrahamyan@medfarsolutions.com>
2022-12-05 12:26:21 +0400

    Add project files.

---
+++ DI_Console/DefaultOperation.cs
@@ -0,0 +1,9 @@
+﻿using static System.Guid;
+
+namespace DI_Console
+{
+    public record class DefaultOperation : ITransientOperation, IScopedOperation, ISingletonOperation
+    {
+        public string OperationId { get; } = NewGuid().ToString()[^4..];
+    }
+}
---
+++ DI_Console/OperationLogger.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DI_Console
+{
+    internal class OperationLogger
+    {
+        private readonly ITransientOperation _transientOperation;
+        private readonly IScopedOperation _scopedOperation;
+        private readonly ISingletonOperation _singletonOperation;
+
+        public OperationLogger(ITransientOperation transientOperation, IScopedOperation scopedOperation, ISingletonOperation singletonOperation)
+            => (_transientOperation, _scopedOperation, _singletonOperation) = (transientOperation, scopedOperation, singletonOperation);
+    }
+}
