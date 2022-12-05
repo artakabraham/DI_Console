768496dcd41c98872cc7d57476ce7fd9c255e386
Artak Abrahamyan <Artak.Abrahamyan@medfarsolutions.com>
2022-12-05 13:25:37 +0400

    Register services for DI

--- DI_Console/Program.cs
+++ DI_Console/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using DI_Console;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+using IHost host = Host.CreateDefaultBuilder(args)
+      .ConfigureServices((_, services) =>
+      services.AddTransient<ITransientOperation, DefaultOperation>()
+              .AddScoped<IScopedOperation, DefaultOperation>()
+              .AddSingleton<ISingletonOperation, DefaultOperation>()
+              .AddTransient<OperationLogger>())
+      .Build();
+
+ExemplifyScoping(host.Services, "Scope 1");
+ExemplifyScoping(host.Services, "Scope 2");
+
+await host.RunAsync();
+
+
+static void ExemplifyScoping(IServiceProvider service, string scope)
+{
+    using IServiceScope serviceScope = service.CreateScope();
+    IServiceProvider provider = serviceScope.ServiceProvider;
+
+    OperationLogger logger = provider.GetRequiredService<OperationLogger>();
+    logger.LogOperations($"{scope}-Call 1 .GetRequiredService<OperationLogger>()");
+
+    Console.WriteLine("...");
+
+    logger = provider.GetRequiredService<OperationLogger>();
+    logger.LogOperations($"{scope}-Call 2 .GetRequiredService<OperationLogger>()");
+
+    Console.WriteLine();
+}
