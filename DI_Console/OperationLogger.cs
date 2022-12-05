c55693ff2460626b1b8239a96a364a9e9014e3c1
Artak Abrahamyan <Artak.Abrahamyan@medfarsolutions.com>
2022-12-05 12:54:42 +0400

    Add service that requires DI

--- DI_Console/OperationLogger.cs
+++ DI_Console/OperationLogger.cs
@@ -14,5 +14,17 @@ namespace DI_Console
 
         public OperationLogger(ITransientOperation transientOperation, IScopedOperation scopedOperation, ISingletonOperation singletonOperation)
             => (_transientOperation, _scopedOperation, _singletonOperation) = (transientOperation, scopedOperation, singletonOperation);
+
+        public void LogOperations(string scope)
+        {
+            LogOperation(_transientOperation, scope, "Always different");
+            LogOperation(_scopedOperation, scope, "Changes only with scope");
+            LogOperation(_singletonOperation, scope, "Always the same");
+        }
+
+        private static void LogOperation<T>(T operation, string scope, string message)
+            where T : IOperation =>
+            Console.WriteLine($"{scope}: {typeof(T).Name,-19} [ {operation.OperationId}...{message,-23} ]");
     }
+}
 }
